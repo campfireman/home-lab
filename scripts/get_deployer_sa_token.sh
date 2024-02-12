0839def6cc724502b3a54bf2a512a998e5400e67
Ture Claussen <ture.cl.business@gmail.com>
2024-02-12 20:17:29 +0100

    Added quiet mode to SA token retrieval

--- scripts/get_deployer_sa_token.sh
+++ scripts/get_deployer_sa_token.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -t ture@elmaestro "sudo kubectl get secret deployer-service-account-token -o jsonpath='{.data.token}' | base64 -d"
+ssh -q -t ture@elmaestro "sudo kubectl get secret deployer-service-account-token -o jsonpath='{.data.token}' | base64 -d"
