150ece9013f604f90925a3752c2f9e8787199efa
Ture Claussen <ture.cl.business@gmail.com>
2024-09-11 13:21:16 +0200

    Added ollama deployment

---
+++ terraform/ollama.tf
@@ -0,0 +1,156 @@
+locals {
+  ollama_name = "ollama"
+  ollama_port = 11434
+}
+
+resource "kubernetes_namespace" "ollama_namespace" {
+  metadata {
+    name = local.ollama_name
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "ollama_pvc" {
+  metadata {
+    name      = "${local.ollama_name}-pvc"
+    namespace = kubernetes_namespace.ollama_namespace.metadata.0.name
+  }
+  spec {
+    access_modes       = ["ReadWriteOnce"]
+    storage_class_name = "longhorn"
+    resources {
+      requests = {
+        storage = "30Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "ollama_deployment" {
+  metadata {
+    name      = "${local.ollama_name}-deployment"
+    namespace = kubernetes_namespace.ollama_namespace.metadata.0.name
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = local.ollama_name
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = local.ollama_name
+        }
+      }
+      spec {
+        container {
+          name              = "${local.ollama_name}-container"
+          image             = "ollama/ollama:0.3.10"
+          image_pull_policy = "IfNotPresent"
+          port {
+            container_port = local.ollama_port
+          }
+
+          #   resources {
+          #     requests = {
+          #       cpu    = "1000m"
+          #       memory = "16Gi"
+          #     }
+          #   }
+
+          lifecycle {
+            post_start {
+              exec {
+                command = ["/bin/sh", "-c", "/bin/ollama pull gemma2"]
+              }
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path   = "/"
+              port   = local.ollama_port
+              scheme = "HTTP"
+            }
+            initial_delay_seconds = 5
+            period_seconds        = 15
+          }
+          readiness_probe {
+            http_get {
+              path   = "/"
+              port   = local.ollama_port
+              scheme = "HTTP"
+            }
+            initial_delay_seconds = 5
+            period_seconds        = 10
+          }
+          volume_mount {
+            name       = "${local.ollama_name}-data"
+            mount_path = "/root/.ollama"
+            sub_path   = "data"
+          }
+        }
+        volume {
+          name = "${local.ollama_name}-data"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.ollama_pvc.metadata.0.name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "ollama_service" {
+  metadata {
+    name      = "${local.ollama_name}-service"
+    namespace = kubernetes_namespace.ollama_namespace.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.ollama_deployment.spec.0.template.0.metadata.0.labels.app
+    }
+    port {
+      port        = 80
+      target_port = local.ollama_port
+      name        = "http"
+      protocol    = "TCP"
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "ollama_ingress" {
+  metadata {
+    name      = "${local.ollama_name}-ingress"
+    namespace = kubernetes_namespace.ollama_namespace.metadata.0.name
+    annotations = {
+      "kubernetes.io/ingress.class"                      = "traefik"
+      "cert-manager.io/cluster-issuer"                   = "internal-issuer"
+      "traefik.ingress.kubernetes.io/router.middlewares" = "kube-system-redirect-https@kubernetescrd"
+    }
+  }
+  spec {
+    rule {
+      host = "${local.ollama_name}.${local.new_domain}"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.ollama_service.metadata.0.name
+              port {
+                name = "http"
+              }
+            }
+          }
+        }
+      }
+    }
+    tls {
+      secret_name = "${local.ollama_name}-tls"
+      hosts       = ["${local.ollama_name}.${local.new_domain}"]
+    }
+  }
+}
