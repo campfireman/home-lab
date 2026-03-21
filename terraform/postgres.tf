d360c4dd3b092e856136d87589b09f857f0a67c4
Ture Claussen <ture.cl.business@gmail.com>
2026-03-21 14:10:57 +0100

    Added postgres backup cronjob

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -2,6 +2,7 @@ locals {
   postgres_name                  = "postgres"
   postgres_port                  = 5432
   postgres_shared_data_directory = "postgres_shared_database"
+  postgres_version               = "18.3"
 }
 
 resource "kubernetes_namespace" "postgres" {
@@ -104,7 +105,7 @@ resource "kubernetes_stateful_set" "postgres" {
       spec {
         container {
           name              = local.postgres_name
-          image             = "postgres:18.3"
+          image             = "postgres:${local.postgres_version}"
           image_pull_policy = "IfNotPresent"
 
           port {
@@ -133,6 +134,89 @@ resource "kubernetes_stateful_set" "postgres" {
           name = "postgres-pv"
           persistent_volume_claim {
             claim_name = kubernetes_persistent_volume_claim.postgres_pvc.metadata.0.name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_cron_job_v1" "postgres_backup" {
+  metadata {
+    name      = "${local.postgres_name}-backup"
+    namespace = kubernetes_namespace.postgres.metadata[0].name
+  }
+
+  spec {
+    schedule = "0 0 * * *"
+    job_template {
+      metadata {
+        name      = "${local.postgres_name}-backup"
+        namespace = kubernetes_namespace.postgres.metadata[0].name
+      }
+      spec {
+        template {
+          metadata {
+            name = "${local.postgres_name}-backup"
+          }
+
+          spec {
+            restart_policy = "OnFailure"
+
+            container {
+              name  = "${local.postgres_name}-backup"
+              image = "postgres:${local.postgres_version}"
+
+              env_from {
+                secret_ref {
+                  name = kubernetes_secret.postgres_secrets.metadata.0.name
+                }
+              }
+
+              env_from {
+                config_map_ref {
+                  name = kubernetes_config_map.postgres_config.metadata.0.name
+                }
+              }
+
+              env {
+                name  = "PGPASSWORD"
+                value_from {
+                  secret_key_ref {
+                    name = kubernetes_secret.postgres_secrets.metadata.0.name
+                    key  = "POSTGRES_PASSWORD"
+                  }
+                }
+              }
+
+              command = [
+                "/bin/sh",
+                "-c",
+                <<-EOT
+                set -e 
+                FILE_NAME="/mnt/backup/backup-$(date +%Y-%m-%d-%H%M).sql.gz"
+                
+                echo "Starting backup..."
+                pg_dumpall -h ${kubernetes_service.postgres_service.metadata.0.name} -U "$POSTGRES_USER" | gzip > "$FILE_NAME"
+                
+                echo "Cleaning up old backups (keeping 5)..."
+                cd /mnt/backup && ls -t backup-*.sql.gz | tail -n +6 | xargs -r rm
+                EOT
+              ]
+
+              volume_mount {
+                name       = "zimaboard-nfs-postgres-backup"
+                mount_path = "/mnt/backup"
+              }
+            }
+
+            volume {
+              name = "zimaboard-nfs-postgres-backup"
+              nfs {
+                server = "192.168.1.67"
+                path   = "/var/nfs/backups/postgres"
+              }
+            }
           }
         }
       }
