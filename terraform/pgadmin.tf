832d4b61a75fcce8515f6ff4b8b14e2e3a10dea4
Ture <ture.cl.business@gmail.com>
2026-09-17 14:02:13 +0000

    Update dpage/pgadmin4 Docker tag to v9.18.0

--- terraform/pgadmin.tf
+++ terraform/pgadmin.tf
@@ -96,7 +96,7 @@ resource "kubernetes_deployment_v1" "pgadmin" {
 
         container {
           name              = "pgadmin"
-          image             = "dpage/pgadmin4:9.15.0"
+          image             = "dpage/pgadmin4:9.18.0"
           image_pull_policy = "IfNotPresent"
 
           env {
